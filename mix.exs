cf48129766b07cb9acf82b261bb09149ec9a4d2a
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2016-07-11 23:58:02 +0200

    Mix.exs update

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule FunLand.Mixfile do
 
   def project do
     [app: :fun_land,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -28,9 +28,8 @@ defmodule FunLand.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:dialyxir, "~> 0.3", only: [:dev]},
       {:currying, "~> 1.0"},
-
+      {:dialyxir, "~> 0.3", only: [:dev]},
     ]
   end
 end
