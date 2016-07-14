79d8edd7d47b39772d53f02116c9f095c2108ec9
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2016-07-14 18:49:52 +0200

    Add Sum and Product as CombinableMonads.

--- lib/fun_land/builtin/map.ex
+++ lib/fun_land/builtin/map.ex
@@ -1,3 +1,38 @@
 defmodule FunLand.Builtin.Map do
+  use FunLand.Mappable
+
+  @moduledoc """
+  Maps are Mappable, because we can map over `{k, v}`-tuples to create new values.
+
+  Maps are however _not_ Appliable, as there is no clearly 
+
+
+  Maps are _not_ Applicative, as there is no way to put 'a value' inside a map (we don't know what key to associate it with)
+  so we cannot define an implementation of `wrap/2`.
   
+  --------------------
+
+  Maps _are_ Combinable, as we can take two key-value pairs, and combine them. 
+  It is also possible to create a `neutral` element: an empty map.
+  """
+
+  def map(map, function) do
+    :maps.map(fn k, v -> function.({k, v}) end, map)
+  end
+
+  use FunLand.Combinable
+
+  def combine(map_a, map_b) do
+    :maps.merge(map_a, map_b)
+  end
+
+  def neural do
+    :maps.new
+  end
+
+  use FunLand.Reducable
+
+  def reduce(map, initial, folding_function) do
+    :maps.fold(fn k, v, acc -> folding_function.({k, v}, acc) end, initial, map)
+  end
 end
---
+++ lib/fun_landic/product.ex
@@ -0,0 +1,54 @@
+defmodule FunLandic.Product do
+  @moduledoc """
+  `Product` wraps numbers. Each `Product` struct is a wrapper around a single number, and basically says:
+  'I want to treat this number as something that, when combined with another number, should be multiplied, using `1` as neutral element'
+
+  This is necessary as there are multiple ways to combine numbers. Another, very common way is `FunLandic.Sum`.
+  
+  You can use `Product` either as a Combinable, by just passing in a Reducable with numbers,
+
+  Or you can use `Product` as Monad, by wrapping individual numbers and combining them using `map`, `apply_with`, `wrap` and `chain`.
+  """
+  use FunLand.CombinableMonad
+
+  defstruct [:val]
+  alias __MODULE__
+
+  def neutral, do: 1
+  def combine(a, b), do: Kernel.*(a, b)
+
+
+
+  def map(%Product{val: val}, function) do
+    %Product{val: function.(val) |> assert_value_is_number}
+  end
+
+  def apply_with(%Product{val: fun}, %Product{val: val}) do
+    %Product{val: fun.(val) |> assert_value_is_number}
+  end
+
+  @doc """
+  The only things that make sense to put in a %Product{} are a number,
+  or a function that will eventually evaluate to a number.
+
+  Note that unfortunately, Elixir has no way to check the output of a function.
+  So we allow 'any' function to be wrapped.
+  """
+  def wrap(val) when is_number(val) or is_function(val) do
+    %Product{val: val}
+  end
+
+  def chain(%Product{val: val}, function) do
+    function.(val)
+  end
+
+
+  defp assert_value_is_number(value) do
+    if is_number(value) do
+      value
+    else
+      raise "non-numeric value passed to FunLandic.Product!"
+    end
+  end
+
+end
